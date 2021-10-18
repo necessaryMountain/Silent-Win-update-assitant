acf4ea33891a2cc4eeb79063acdcf8c6a41e305d
necessaryMountain <83309232+necessaryMountain@users.noreply.github.com>
2021-10-18 19:37:10 -0400

    The exe is just the script wrapped with ps2exe
    The script just runs the update assistant silently.  Good for updating a batch of computers to the newest windows 10 feature update.

---
+++ Windows Upgrade Assistant Script.ps1
@@ -0,0 +1,25 @@
+﻿#Windows upgrade script found on spiceworks.com from user mikehanseman.  Full credit goes to them, though I know there are other similar scripts out there.
+
+#creates a directory to store the Windows update Assistant
+$dir = 'C:\packages'
+
+#makes the defined directory, but doesn't overwrite an existing directory
+mkdir $dir
+
+#creates a new object using the WebClient class
+$webClient = New-Object System.Net.WebClient
+
+#defines the url for downloading the windows update assistant
+$url = 'https://go.microsoft.com/fwlink/?LinkID=799445'
+
+#defining a full file path to store the upgrade assistant
+$file = "$($dir)\Windows10Upgrade9252.exe"
+
+#downloading the file from that url using the defined path
+$webClient.DownloadFile($url,$file)
+
+#Starting the upgrade assistant
+Start-Process -FilePath $file -ArgumentList '/quietinstall /skipeula /auto upgrade /copylogs $dir'
+
+#possible flags for the update assistant
+#/SkipCompatCheck /ShowProgressInTaskBarIcon /NoRestartUI 
